eae561f6e1da0354891b05636e13424145d38c4c
Harini <harinip@surinova.com>
2026-03-24 19:17:24 +0530

    24/3/26

--- FOC_model_1/PMSM_to_BLDC_FOC.m
+++ FOC_model_1/PMSM_to_BLDC_FOC.m
@@ -1,5 +1,9 @@
 % Parameters
 pwm_frequency = 20e3;
+x=60;
+rps=x;
+f_speed= pwm_frequency/100;
+f_current=pwm_frequency/10
 %Bldc RPP parameters
 Ld = 0.0017;
 Lq = 0.0018;
@@ -11,10 +15,11 @@ J = 7e-6;
 B = 0.0025;
 Damping_viscous = 0;
 %Bldc RPP parameters
-omg_cl = 2 * 3.14 * pwm_frequency;
-Kp_Id = omg_cl * Ld;
-Ki_Id = omg_cl * R_s;
-Kp_Iq = omg_cl * Lq;
-Ki_Iq = omg_cl * R_s;
-kp_speed = ((J*omg_cl)/(1.5*motor_polepairs*flux_linkage)) ;
-ki_speed = ((B *omg_cl * omg_cl)/(1.5*motor_polepairs*flux_linkage)) ;
+omg_cls = 2 * 3.14 * f_speed;
+omg_clc = 2 * 3.14 * f_current;
+Kp_Id = omg_clc * Ld;
+Ki_Id = omg_clc * R_s;
+Kp_Iq = omg_clc * Lq;
+Ki_Iq = omg_clc * R_s;
+kp_speed = ((J*omg_cls)/(1.5*motor_polepairs*flux_linkage)) ;
+ki_speed = ((B *omg_cls * omg_cl)/(1.5*motor_polepairs*flux_linkage)) ;
